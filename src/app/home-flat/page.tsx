6a04b3b9437515609569c617acc3f42247bbb38a
Damshire <damian.ay.thompson@gmail.com>
2026-03-06 05:19:41 -0500

    perf: make home flat page static

--- src/app/home-flat/page.tsx
+++ src/app/home-flat/page.tsx
@@ -1,7 +1,5 @@
 import { FlatHomePage } from "@/components/home/FlatHomePage";
 
-export const dynamic = "force-dynamic";
-
 export default async function HomeFlatPage() {
   return <FlatHomePage />;
 }
